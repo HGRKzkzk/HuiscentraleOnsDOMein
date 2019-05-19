45f3a144d4a138f677852c8e94d0df563832e15c
ANNA VDLINDE <annavdlinde@gmail.com>
2019-05-19 11:42:13 +0200

    Initial commit. Huiscentrale in eerste vorm: 1. gebruikt het protocol nog zoals ik dacht dat het zat set/get;afzender;ontvanger;pin:waarde 2. bericht terug is bericht dat naar HC gestuurd is, uitlezen van Arduino moet nog geimplementeerd

---
+++ src/Huiscentrale.java
@@ -0,0 +1,115 @@
+import com.fazecast.jSerialComm.SerialPort;
+import com.onsdomein.proxy.ProxyOnsDomein;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+class Huiscentrale {
+    private String client_id = "5678";
+    private ProxyOnsDomein proxy = new ProxyOnsDomein();
+    private OutputStream output;
+    private InputStream input;
+    private String portLinks = "COM3";
+    private SerialPort port = SerialPort.getCommPort(portLinks);
+
+    Huiscentrale() {
+        // After first boot of app connection is made with both arduino and server, then passes on to listeningForMessage
+        setupArduinoConnection();
+
+        try {
+            proxy.connectClientToServer(client_id);
+            System.out.println("Connected to server");
+            listenForMessageFromServer();
+        } catch (IOException e) {
+            System.out.println("HC cannot connect to server: " + e);
+        }
+    }
+
+
+    private void listenForMessageFromServer() {
+        // get messages from server
+        while (true) {
+            String request;
+            try {
+                request = proxy.receiveRequest();
+            } catch (Exception e) {
+                System.out.println("Connection with server lost. " + e);
+                break;
+            }
+            System.out.println("received from server: " + request);
+            sendToArduino(request);
+        }
+    }
+
+    private void setupArduinoConnection() {
+        // setup of the serial connection to the Arduino
+
+        try {
+            // open the serialport, serialport can only be opened if there is no other program using (Arduino IDE for example).
+            port.openPort();
+            System.out.println("Succesfully opened the port.");
+            port.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+            // Create an outputstream.
+            output = port.getOutputStream();
+            //create an inputstream.
+            input = port.getInputStream();
+            //set the baudrate to the same as the arduino
+            port.setBaudRate(57600);
+            System.out.println("OutputStream established.");
+            System.out.println("InputStream established.");
+        } catch (Exception e) {
+            System.out.println("Comport niet beschikbaar");
+            System.exit(1);
+        }
+    }
+
+    private void sendToArduino(String message) {
+        String reactionFromArduino;
+        String[] messageSplit = message.split(";", 0);
+        //checks if the message has the correct format
+        if (messageSplit.length == 3) {
+            String actionType = "";
+
+            if (messageSplit[0].equals("setHc")) {
+                actionType = "1";
+            } else if (messageSplit[0].equals("getHc")) {
+                actionType = "2";
+            }
+
+            String outputToArduino = "<" + actionType + ":" + messageSplit[2] + ">";
+
+            System.out.println("Sending to Arduino: " + outputToArduino);
+            try {
+                output.write(outputToArduino.getBytes());
+                System.out.println("sending serial message to arduino succesfull");
+            } catch (IOException e) {
+                System.out.println("sending serial message to arduino failed");
+            }
+
+            //TODO: handle response from Arduino
+
+
+//            reactionFromArduino = "Arduino received your message and says hi!";
+
+            reactionFromArduino = messageSplit[2];
+
+        } else {
+            reactionFromArduino = "res;FAIL: No message send to Arduino";
+        }
+
+        sendToProxy(messageSplit[1], reactionFromArduino);
+
+    }
+
+    private void sendToProxy(String reactionFor, String reactionFromArduino) {
+        try {
+            //TODO: make sure you always respond, the server will if HC is offline, GA waits for a reply from either server or HC
+            proxy.sendResponse("setHc", client_id, reactionFor, reactionFromArduino);
+        } catch (Exception e) {
+            System.out.println("HC kan geen contact maken met de server. " + e);
+        }
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    //only serves to create an instance of the Huiscentrale
+    public static void main(String[] args) {
+        Huiscentrale huiscentrale1 = new Huiscentrale();
+        }
+}
